e8e39644e7251c892e1ddfeda0d3619927259a0c
hongjunpyo11 <jpyo98@naver.com>
2023-01-17 15:51:50 +0900

    테스트 케이스 추가

--- src/test/java/com/ll/exam/springsecurityjwt/SpringsecurityJwtApplicationTests.java
+++ src/test/java/com/ll/exam/springsecurityjwt/SpringsecurityJwtApplicationTests.java
@@ -1,13 +1,16 @@
 package com.ll.exam.springsecurityjwt;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -49,4 +52,37 @@ class SpringsecurityJwtApplicationTests {
 				.andExpect(status().is2xxSuccessful());
 	}
 
+	@Test
+	@DisplayName("POST /member/login 으로 올바른 username과 password 데이터를 넘기면 JWT키를 발급해준다.")
+	void t2() throws Exception {
+		// When
+		ResultActions resultActions = mvc
+				.perform(
+						post("/member/login")
+								.content("""
+                                        {
+                                            "username": "user1",
+                                            "password": "1234"
+                                        }
+                                        """.stripIndent())
+								.contentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8))
+				)
+				.andDo(print());
+
+		// Then
+		resultActions
+				.andExpect(status().is2xxSuccessful());
+
+		MvcResult mvcResult = resultActions.andReturn();
+
+		MockHttpServletResponse response = mvcResult.getResponse();
+
+		String authentication = response.getHeader("Authentication");
+
+		Assertions.assertThat(authentication).isNotEmpty();
+	}
+
+	/**
+	 * POST /member/login 의 응답 헤더에는 Authentication 값이 있어야 합니다.
+	 */
 }
