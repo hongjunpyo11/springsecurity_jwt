0af958758256cf62e06a112183332cd0bf9dd24c
hongjunpyo11 <jpyo98@naver.com>
2023-01-20 22:07:02 +0900

    LoginDto 클래스를 별도의 파일로, GET /member/me 구현

--- src/main/java/com/ll/exam/springsecurityjwt/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/springsecurityjwt/app/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.ll.exam.springsecurityjwt.app.member.controller;
 
 import com.ll.exam.springsecurityjwt.app.base.dto.RsData;
+import com.ll.exam.springsecurityjwt.app.member.dto.request.LoginDto;
 import com.ll.exam.springsecurityjwt.app.member.entity.Member;
 import com.ll.exam.springsecurityjwt.app.member.service.MemberService;
 import com.ll.exam.springsecurityjwt.app.security.entity.MemberContext;
@@ -27,6 +28,15 @@ public class MemberController {
     @GetMapping("/test")
     public String test(@AuthenticationPrincipal MemberContext memberContext) {
         return "안녕" + memberContext;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<RsData> me(@AuthenticationPrincipal MemberContext memberContext) {
+        if (memberContext == null) { // 임시코드, 나중에는 시프링 시큐리티를 이용해서 로그인을 안했다면, 아예 여기로 못 들어오도록
+            return Util.spring.responseEntityOf(RsData.failOf(null));
+        }
+
+        return Util.spring.responseEntityOf(RsData.successOf(memberContext));
     }
 
     @PostMapping("/login")
@@ -60,15 +70,5 @@ public class MemberController {
                 ),
                 Util.spring.httpHeadersOf("Authentication", accessToken)
         );
-    }
-
-    @Data
-    public static class LoginDto {
-        private String username;
-        private String password;
-
-        public boolean isNotValid() {
-            return username == null || password == null || username.trim().length() == 0 || password.trim().length() == 0;
-        }
     }
 }
