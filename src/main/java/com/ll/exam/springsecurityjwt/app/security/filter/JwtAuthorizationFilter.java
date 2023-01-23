ea7bb31f51a3c476cce11179439c44802b62d032
hongjunpyo11 <jpyo98@naver.com>
2023-01-23 21:57:13 +0900

    CPU 연산만으로 회원인증 및 회원정보 로딩

--- src/main/java/com/ll/exam/springsecurityjwt/app/security/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/ll/exam/springsecurityjwt/app/security/filter/JwtAuthorizationFilter.java
@@ -1,7 +1,6 @@
 package com.ll.exam.springsecurityjwt.app.security.filter;
 
 import com.ll.exam.springsecurityjwt.app.member.entity.Member;
-import com.ll.exam.springsecurityjwt.app.member.service.MemberService;
 import com.ll.exam.springsecurityjwt.app.security.entity.MemberContext;
 import com.ll.exam.springsecurityjwt.app.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -26,7 +24,6 @@ import java.util.Map;
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
     private final JwtProvider jwtProvider;
-    private final MemberService memberService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
@@ -37,11 +34,8 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
             if (jwtProvider.verify(token)) {
                 Map<String, Object> claims = jwtProvider.getClaims(token);
-                String username = (String) claims.get("username");
-                Member member = memberService.findByUsername(username).orElseThrow(
-                        () -> new UsernameNotFoundException("'%s' Username not found.".formatted(username))
-                );
 
+                Member member = Member.fromJwtClaims(claims);
                 forceAuthentication(member);
             }
         }
